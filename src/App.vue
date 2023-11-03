c0f02ddfa0a18a11cdc737597be0d4cb74342ce1
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-11-03 16:03:26 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data() {
+    return {
+      message: 'La mia prima app con Vite!'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style scoped>
+</style>
